8782e22460a69cd91d14bdafcfabc15f8278f94b
CarolineKaufman <ckauf11@lsu.edu>
2018-10-22 15:26:54 -0700

    Week 9 assignments

---
+++ assignments/Week9Pop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Set Starting Population Number
+
+# Set Number Offspring Variation~ Poisson
+
+# Deaths (poisson for time)
+
+# Population at a given time
+
+# Set Carrying Capacity
+
+# Population over Time
