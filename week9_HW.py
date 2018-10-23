18d3c281b806e74d9723c85faf5fcb2522c43a9f
Zahmed4 <zahmed4@lsu.edu>
2018-10-22 23:06:26 -0700

    week 9 homework script

---
+++ week9_HW.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# Set a starting number of a population.
+# Figure out number of offspring per individual with poisson
+# Set a number for carrying capacity
+# Loop this for several generationsto determine population size changes 
