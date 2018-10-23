207e57ac15f5777fa94a8a2beb5e78051578765d
Zahmed4 <zahmed4@lsu.edu>
2018-10-22 23:03:41 -0700

    InClass script assignment

---
+++ assignments/week9_InClass.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#will use sys for command-line arguments
+#setting up dictionary
+Codon2AA = {"AAA":"Lysine ", "AAC":"Asparagine ", "AAG":"Lysine ", "AAU":"Asparagine ",
+"ACA":"Threonine ", "ACC":"Threonine ", "ACG":"Threonine ", "ACU":"Threonine ",
+"AGA":"Arginine ", "AGC":"Serine ", "AGG":"Arginine ", "AGU":"Serine ",
+"AUA":"Isoleucine ", "AUC":"Isoleucine ", "AUG":"Methionine ", "AUU":"Isoleucine ",
+"CAA":"Glutamine ", "CAC":"Hisdadine ", "CAG":"Glutamine ", "CAU":"Hisdadine ",
+"CCA":"Proline ", "CCC":"Proline ", "CCG":"Proline ", "CCU":"Proline ",
+"CGA":"Arginine ", "CGC":"Arginine ", "CGG":"Arginine ", "CGU":"Arginine ",
+"CUA":"Leucine ", "CUC":"Leucine ", "CUG":"Leucine ", "CUU":"Leucine ",
+"GAA":"Glutamate ", "GAC":"Aspartate ", "GAG":"Glutamate ", "GAU":"Aspartate ",
+"GCA":"Alanine ", "GCC":"Alanine ", "GCG":"Alanine ", "GCU":"Alanine ",
+"GGA":"Glycine ", "GGC":"Glycine ", "GGG":"Glycine ", "GGU":"Glycine ",
+"GUA":"Valine ", "GUC":"Valine ", "GUG":"Valine ", "GUU":"Valine ",
+"UAA":"Stop ", "UAC":"Tyrosine ", "UAG":"Stop ", "UAU":"Threonine ",
+"UCA":"Serine ", "UCC":"Serine ", "UCG":"Serine ", "UCU":"Serine ",
+"UGA":"Stop ", "UGC":"Cysteine ", "UGG":"Tryptophan ", "UGU":"Cysteine ",
+"UUA":"Leucine ", "UUC":"Phenylalanine ", "UUG":"Leucine ", "UUU":"Phenylalanine "}
+import sys
+#Starting list for reading multiple files
+l = []
+user_input = sys.argv[1:]
+for filename in user_input:
+        with open(filename, 'r') as myfile:
+                myLine = myfile.read()
+        l.append(myLine)
+dna = ''.join(l)
+dna = dna.upper()
+rnaSeq = dna.replace("T","U")
+#print("DNA sequences transcribed: %s" %rnaSeq)
+#Converts the RNA sequence to amino acids.
+proteinSeq = ""
+for i in range(0, len(rnaSeq), 3):
+	if rnaSeq[i:i+3] in Codon2AA:
+		proteinSeq += Codon2AA[rnaSeq[i:i+3]]
+#print ("Protein Sequence: "+proteinSeq)
+print("The transcribed sequences to amino acids are located in the file named Protein_Sequence.")
+outFileName = "Protein_Sequence.txt"
+outFile = open(outFileName,'w')
+outFile.write("%s \n" %proteinSeq)
+
+
