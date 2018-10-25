3df02ab3fb42dec3e47890c8dffe6dd4c9589b20
CompBio <Cameron F>
2018-10-25 07:32:44 -0700

    Commit files PopSim and PopSimSummary

--- PopSim.py
+++ PopSim.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 #this will rename and import all needed functions
-	import matplotlib as plt
-	import numpy.random as nr
+import matplotlib as plt
+
+import numpy.random as nr
+
 #this will allow input of carrying cap
-	Popsize = int(input("Select the Carrying Cap:   "))
-#This will produce an array with average offspring 5 pop size 100
-	pop = nr.poisson(lam=5,Popsize)
-	print pop
+Popsize = int(input("Select the PopulationSize:   "))
+
+Gensize = int(input("Select the generationSize:   "))
+
+#This will produce an array with average offspring 5 for some gen size
+for i in range (0, Gensize) 
+pop = nr.poisson(lam=5.0, size=Popsize)
+print(pop)
+
+#To poisson for next generation
+nextpop = 0
+nextpop += nr.poisson(lam=5.0, size=pop)
+
+#To prevent pop from exceeding Carrying cap
+	if nextpop > lam=5.0
+	nextpop = lam=5.0
