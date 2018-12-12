189445801ce87208b5257cbc6a9db7601f12d885
jembrown <jeremymbrown@gmail.com>
2018-12-11 22:42:37 -0600

    Comments for CK and WH

--- assignments/CarolineKaufman/Week9HW.py
+++ assignments/CarolineKaufman/Week9HW.py
@@ -1,23 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python		# DB: More robust to use /usr/bin/env python
 
 # t(number of generations) and K(carrying capacity) must be whole numbers
 import numpy as np
 import numpy.random as nr
 import matplotlib.pyplot as plt
-#set imput for population formula with starting population, generations and max capacity. 
-r = nr.poisson()
+
+#set input for population formula with starting population, generations and max capacity. 
+r = nr.poisson(2.0)	# DB: I'd suggest using a poisson with a lambda higher than 1.
 K = input('Set Carrying Capacity:')
 t = input('Set Number of Generations:')
 p = input('Set Starting Population:')
+
 t = int(t)
 K = int(K)
 r = float(r)
 p = int(p)
+
 #set starting number and popultion
 num = [p]*(t+1)
+
 #log. growth population equation model 
 for i in range(t): 
     num[i+1] = num[i]+r*num[i]*(1-num[i]/K)
+
 # graphing results with plot
 plt.plot(range(t+1),num, 'b')
 plt.xlabel('Generation')
@@ -25,3 +30,11 @@ plt.ylabel('Number')
 plt.title('Growth rate: %s, Carrying Capacity = %d' % (r, K))
 plt.axvline(np.argmax(np.diff(num)),  color = 'k' )
 plt.show() 
+
+# DB: Overall, this looks pretty good. I had been envisioning an individual-level 
+#     simulation, where the number of offspring was drawn independently from a Poisson for 
+#     each individual. But using the analytical equation should produce a similar result, albeit 
+#     with less variation from generation to generation and more variation across runs. The single 
+#     value of r drawn above will have a large influence on the results.
+
+# DB: Did you include a writeup of the results you observed?
--- assignments/whurst1/PopulationSimulation.py
+++ assignments/whurst1/PopulationSimulation.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python
-
-import numpy
+#!/usr/bin/env python		# DB: More robust if you use this shebang. For me, using /usr/bin/python
+							# calls v2, but /usr/bin/env python. In this case, it changes the results
+import numpy				# because of the way the versions handle conversion of ints to floats.
 import numpy.random as NumRan
 import matplotlib.pyplot as plot
+
 #choose random offspring rate
 Rate= NumRan.poisson(2.0)
+
 #set max pop size
 MaxPop= input("Enter the Max Population: ")
+
 #Number of generations to run
 Gens= input("Enter the Number of Generations to run: ")
 StartPop= input("Enter Starting Pop: ")
+
 #setting
 Gens=int(Gens)
 MaxPop= int(MaxPop)
@@ -19,7 +23,7 @@ StartPop= int(StartPop)
 num = [StartPop+1]*(Gens+1)
 
 for organisms in range(Gens):
-	num[organisms+1] = num[organisms]+Rate*num[organisms]*(1-num[organisms]/MaxPop)
+	num[organisms+1] = num[organisms]+(Rate*num[organisms]*(1-(num[organisms]/MaxPop))) 
 
 plot.plot(range(Gens+1),num, "--")
 plot.xlabel("Generation")
@@ -27,5 +31,18 @@ plot.ylabel("Number")
 plot.title("Growth rate: %s, Carrying Capacity =%d" % (Rate, MaxPop))
 plot.show()
 
-#the growth rate rarely goes above 1 in the simulation. When I try to enter Numpy.random.poisson(2, 10) it gives me:
-#"only length-1 arrays can be converted to Python scalars".
+# the growth rate rarely goes above 1 in the simulation.
+# DB: I'm not sure about this. Drawing from a Poisson with lambda=2 should often give
+#     growth rates above 1.
+
+# When I try to enter Numpy.random.poisson(2, 10) it gives me:
+# "only length-1 arrays can be converted to Python scalars".
+# DB: Did you try: sum(NumRan.poisson(2,10))?
+
+# DB: Overall, this looks pretty good. I had been envisioning an individual-level 
+#     simulation, where the number of offspring was drawn independently from a Poisson for 
+#     each individual. But using the analytical equation should produce a similar result, albeit 
+#     with less variation from generation to generation and more variation across runs. The single 
+#     value of Rate drawn above will have a large influence on the results.
+
+# DB: Did you include a writeup of the results you observed?
